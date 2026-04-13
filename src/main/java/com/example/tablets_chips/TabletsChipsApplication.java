a5fc9c4191ca16a08e7032139558861edbf4ccce
guiantebii <guiantebi2000@gmail.com>
2026-04-12 21:12:15 -0300

    Transformando o projeto em Java Spring

---
+++ src/main/java/com/example/tablets_chips/TabletsChipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tablets_chips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletsChipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletsChipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tablets_chips/TabletsChipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tablets_chips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletsChipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
